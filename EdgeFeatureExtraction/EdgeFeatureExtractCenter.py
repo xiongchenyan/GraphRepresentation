d47ceec254c158f0e4f7b8db16bb8b105ab4f5d6
xiongchenyan <xiongchenyan@gmail.com>
2015-06-10 17:40:05 +0800

    add note

--- EdgeFeatureExtraction/EdgeFeatureExtractCenter.py
+++ EdgeFeatureExtraction/EdgeFeatureExtractCenter.py
@@ -1,16 +1,29 @@
 '''
 Created on my MAC Jun 10, 2015-3:13:59 PM
 What I do:
-I am the center for edge feature extraction
+    I am the center for edge feature extraction
 What's my input:
-qid,query,lDoc,lQObj,lDocObj
+    qid,query,lDoc,lQObj,lDocObj
 What's my output:
-the features between any two
-    call all edge feature extractors to complete the job
-        maybe one center for each of them
+    the features between any two
+        call all edge feature extractors to complete the job
+            maybe one center for each of them
 
 TBD:
     think about how to store these features in a tensor or so
         so that they can be used in numpy+sckipy based graph ranking model
 @author: chenyanxiong
+'''
+
+'''
+pipeline:
+    get input:
+    fill the objid to obj (APIBase etc.)
+    call center for:
+        q-obj
+        doc-obj
+        obj-obj features
+        and pack them into numpy format (TBD:study how)
+    output
+
 '''
