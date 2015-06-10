7be788aebf22512f70bb7e2be0fb59a157cbb474
xiongchenyan <xiongchenyan@gmail.com>
2015-06-10 16:57:16 +0800

    init edge feature center

---
+++ EdgeFeatureExtraction/EdgeFeatureExtractCenter.py
@@ -0,0 +1,16 @@
+'''
+Created on my MAC Jun 10, 2015-3:13:59 PM
+What I do:
+I am the center for edge feature extraction
+What's my input:
+qid,query,lDoc,lQObj,lDocObj
+What's my output:
+the features between any two
+    call all edge feature extractors to complete the job
+        maybe one center for each of them
+
+TBD:
+    think about how to store these features in a tensor or so
+        so that they can be used in numpy+sckipy based graph ranking model
+@author: chenyanxiong
+'''
