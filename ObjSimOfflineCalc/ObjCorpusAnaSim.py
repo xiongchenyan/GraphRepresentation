8a1991f2c05262fd8927003bb2f4d1360076ce76
xiongchenyan <xiongchenyan@gmail.com>
2015-06-11 12:07:52 +0800

    initialize not implemented

---
+++ ObjSimOfflineCalc/ObjCorpusAnaSim.py
@@ -0,0 +1,18 @@
+'''
+Created on my MAC Jun 11, 2015-12:00:57 PM
+What I do:
+    I generate obj-obj->score from corpus ana results
+What's my input:
+    a file, contains the pairwise cnt for target pairs
+    a ctf file for obj ctf in facc | fakba 
+    a score metric choice:
+        default:    \frac{pair cnt}{obj a' ctf}
+What's my output:
+    a pickle dict, obj\tobj score (directed, a->b different with b->a, both should in dict)
+@author: chenyanxiong
+'''
+
+'''
+TBD: waiting for I get the pairwise cnt infor
+'''
+
