1117ce0a6c72735c773fad10b4e442d7651bb723
xiongchenyan <xiongchenyan@gmail.com>
2015-06-15 16:38:35 +0800

    rename

--- EdgeFeatureExtraction/DocObjEdgeFeatureFaccExtractor.py
+++ EdgeFeatureExtraction/DocObjEdgeFeatureFaccExtractor.py
@@ -24,7 +24,7 @@ from EdgeFeatureExtraction.DocObjEdgeFeatureExtractor import DocObjEdgeFeatureEx
 from Facc.FaccDataCenter import FaccDataCenterC
 
 
-class DocObjEdgeFeatureFaccExtractor(DocObjEdgeFeatureExtractorC):
+class DocObjEdgeFeatureFaccExtractorC(DocObjEdgeFeatureExtractorC):
     
     def Init(self):
         DocObjEdgeFeatureExtractorC.Init(self)
