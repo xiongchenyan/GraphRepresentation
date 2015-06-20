68f163f5357a8bb3095cfd2b4f140f8f4c835e6f
xiongchenyan <xiongchenyan@gmail.com>
2015-06-20 10:39:47 +0800

    log level to info

--- EdgeFeatureExtraction/EdgeFeatureExtractCenter.py
+++ EdgeFeatureExtraction/EdgeFeatureExtractCenter.py
@@ -263,7 +263,7 @@ if __name__ == '__main__':
     root.setLevel(logging.DEBUG)
     
     ch = logging.StreamHandler(sys.stdout)
-    ch.setLevel(logging.DEBUG)
+    ch.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     root.addHandler(ch)       
