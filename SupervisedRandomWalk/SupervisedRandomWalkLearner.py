85d87db64cb84fc4f5565b6a3bd88c0d2ad217b4
xiongchenyan <xiongchenyan@gmail.com>
2015-06-22 17:50:15 +0800

    initialize mode learner and predictor

---
+++ SupervisedRandomWalk/SupervisedRandomWalkLearner.py
@@ -0,0 +1,13 @@
+'''
+Created on my MAC Jun 22, 2015-5:33:12 PM
+What I do:
+    I train supervised random walk
+What's my input:
+    graph and label
+    and hyper parameters:
+        
+        
+What's my output:
+    trained parameter
+@author: chenyanxiong
+'''
---
+++ SupervisedRandomWalk/SupervisedRandomWalkPredictor.py
@@ -0,0 +1,11 @@
+'''
+Created on my MAC Jun 22, 2015-5:34:09 PM
+What I do:
+    I predict doc's rank using supervised random walk
+What's my input:
+    graph, and parameter
+What's my output:
+    ranking score for docs
+    (or evaluation results based on what I need)
+@author: chenyanxiong
+'''
