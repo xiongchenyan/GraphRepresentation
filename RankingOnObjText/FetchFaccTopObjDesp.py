bccd2b3030812ad83cc064b87ce25f6b7d0dbb27
xiongchenyan <xiongchenyan@gmail.com>
2015-07-29 21:36:39 -0400

    add ranking on obj text package

---
+++ RankingOnObjText/FetchFaccTopObjDesp.py
@@ -0,0 +1,57 @@
+'''
+Created on my MAC Jul 29, 2015-9:22:33 PM
+What I do:
+    I fetch the most frequent k facc obj's description
+What's my input:
+    facc idf in + all fb desp file + top k + output
+What's my output:
+    the name, id and desp of top k facc obj
+@author: chenyanxiong
+'''
+
+
+import sys,logging
+
+def LoadTopKFaccObjId(InName,k):
+    lObjId = []
+    
+    for line in open(InName):
+        vCol = line.strip().split('\t')
+        if len(vCol) != 2:
+            continue
+        lObjId.append(vCol[0])
+        if len(lObjId) >= k:
+            break
+    return lObjId
+
+
+
+def FetchTargetDesp(DespInName,lObjId,OutName):
+    sObjId = set(lObjId)
+    out = open(OutName,'w')
+    for line in open(DespInName):
+        line = line.strip()
+        vCol = line.split('\t')
+        if vCol[1] in sObjId:
+            print >>out, line
+            
+    out.close()
+    return
+
+
+def Process(DespInName,FaccIdfInName,k,OutName):
+    
+    lObjId = LoadTopKFaccObjId(FaccIdfInName, k)
+    FetchTargetDesp(DespInName, lObjId, OutName)
+    logging.info('done')
+    
+    
+if 5 != len(sys.argv):
+    print 'I fetch facc top obj desp'
+    print 'desp in name, facc idf in, k, out name'
+    sys.exit()
+    
+    
+Process(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])        
+        
+    
---
+++ RankingOnObjText/__init__.py
@@ -0,0 +1,7 @@
+'''
+I contain the exploration code for ranking over object text
+
+possible task:
+    retrieval object (ana obj for query, e.g. millions query track)
+    rank name in obj's description
+'''
