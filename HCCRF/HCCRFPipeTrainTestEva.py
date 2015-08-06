5a019a964483994ad237726f95ed344fb334070c
chenyanxiong <xiongchenyan@gmail.com>
2015-08-06 11:09:05 -0400

    only infor

--- HCCRF/HCCRFPipeTrainTestEva.py
+++ HCCRF/HCCRFPipeTrainTestEva.py
@@ -107,7 +107,7 @@ if __name__ == '__main__':
         sys.exit()
         
     root = logging.getLogger()
-    root.setLevel(logging.DEBUG)
+    root.setLevel(logging.INFO)
     
     ch = logging.StreamHandler(sys.stdout)
 #     ch.setLevel(logging.INFO)
