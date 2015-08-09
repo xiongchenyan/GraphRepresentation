547f266cfda88ae1a9ccf768e63bd215d3c03ca5
chenyanxiong <xiongchenyan@gmail.com>
2015-08-09 16:24:44 -0400

    transfer data

---
+++ GraphFullFeatureExtraction/TransferEsdRankDataToGraphFeature.py
@@ -0,0 +1,60 @@
+'''
+Created on Aug 9, 2015 4:14:34 PM
+@author: cx
+
+what I do:
+    I transfer EsdRank data to graph data
+what's my input:
+    EsdRank data dir + query in
+what's my output:
+    a HCCRF data format dir
+
+'''
+
+
+import site
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+site.addsitedir('/bos/usr0/cx/PyCode/GraphRepresentation')
+
+from HCCRF.DocGraph import DocGraphC
+import logging
+
+import sys,os
+
+if 4 != len(sys.argv):
+    print 'q in + esd rank data dir + out dir'
+    sys.exit()
+    
+    
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+    
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)           
+
+InDir = sys.argv[2]
+OutDir = sys.argv[3]
+    
+lQid = [line.split('\t')[0] for line in open(sys.argv[1]).read().splitlines()]
+
+llGraphData = [DocGraphC.LoadEsdRankOneQGraph(InDir, qid) for qid in lQid]
+
+logging.info('loaded')
+
+llGraphData = DocGraphC.FeatureMinMaxNormalization(llGraphData)
+
+logging.info('normalized')
+
+for qid,lGraphData in zip(lQid,llGraphData):
+    os.makedirs(OutDir + '/' + qid)
+    for GraphData in lGraphData:
+        OutName = OutDir + '/' + qid + '/' + GraphData.DocNo
+        GraphData.dump(OutName)
+        
+ 
+logging.info('dumped')    
+
+
