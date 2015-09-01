b1c4a218604c684611b2cb9c8954069cdb67e28a
chenyanxiong <xiongchenyan@gmail.com>
2015-09-01 17:01:30 -0400

    construct q's serp doc kg's

---
+++ DocGraphRepresentation/ConstructSearchResDocGraph.py
@@ -0,0 +1,109 @@
+'''
+Created on Sep 1, 2015 4:25:44 PM
+@author: cx
+
+what I do:
+    I form doc Kg for given query's search results
+what's my input:
+    query and serp cache
+    resources needed by DocKGUnsupervisedFormer
+    
+
+what's my output:
+    a directory
+        /qid/DocNo
+            each file is the dump of DocKg
+'''
+
+import site
+import logging
+site.addsitedir('/bos/usr0/cx/PyCode/cxPyLib')
+site.addsitedir('/bos/usr0/cx/PyCode/GraphRepresentation')
+
+from cxBase.base import cxBaseC
+from cxBase.Conf import cxConfC
+from IndriSearch.IndriSearchCenter import IndriSearchCenterC
+
+import os
+from DocKGUnsupervisedFormer import DocKGTagMeFormerC,DocKGFaccFormerC,DocKGUnsupervisedFormerC
+
+class SearchResDocGraphConstructorC(cxBaseC):
+    
+    def Init(self):
+        cxBaseC.Init(self)
+        self.Searcher = IndriSearchCenterC()
+        
+        self.GraphFormer = DocKGUnsupervisedFormerC()  #virtual here
+        self.GraphSource = 'tagme'
+        self.OutDir = ""
+        
+        
+    def SetConf(self, ConfIn):
+        cxBaseC.SetConf(self, ConfIn)
+        self.Searcher.SetConf(ConfIn)
+        
+        self.GraphSource = self.conf.GetConf('graphsource',self.GraphSource)
+        
+        if self.GraphSource == 'tagme':
+            self.GraphFormer = DocKGTagMeFormerC(ConfIn)
+        if self.GraphSource == 'facc':
+            self.GraphFormer = DocKGFaccFormerC(ConfIn)
+        
+        if not self.GraphSource in set(['tagme','facc']):
+            logging.error('graph formmer type [%s] not supported',self.GraphSource)
+            raise NotImplementedError
+        
+        
+    def FormForOneQ(self,qid,query):
+        lDoc = self.Searcher.RunQuery(query, qid)
+        
+        lDocKg = [self.GraphFormer.FillDocGraph(doc.DocNo) for doc in lDoc]
+        
+        QueryOutDir = self.OutDir + '/' + qid
+        if not os.path.exists(QueryOutDir):
+            os.makedirs(QueryOutDir)
+            
+        for DocKg in lDocKg:
+            DocKg.dump(QueryOutDir + '/' + DocKg.DocNo)
+            logging.debug('[%s] dummped [%d] node',DocKg.DocNo,len(DocKg))
+            
+        logging.info('[%s-%s] doc kg formed',qid,query)
+        return True
+    
+    
+    def Process(self,QInName):
+        lQidQuery = [line.split('\t') for line in open(QInName).read().splitlines()]
+        
+        for qid,query in lQidQuery:
+            self.FormForOneQ(qid, query)
+            
+        logging.info('[%s] query finished',QInName)
+        return True
+    
+if __name__ == '__main__':
+    import sys
+    if 2 != len(sys.argv):
+        print 'I construct query search result doc kg'
+        SearchResDocGraphConstructorC.ShowConf()
+        sys.exit()
+        
+    root = logging.getLogger()
+    root.setLevel(logging.INFO)
+    
+    ch = logging.StreamHandler(sys.stdout)
+#     ch.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    root.addHandler(ch)          
+    
+    Processor = SearchResDocGraphConstructorC(sys.argv[1])
+    
+    conf = cxConfC(sys.argv[1])
+    QInName = conf.GetConf('in', '/bos/usr0/cx/tmp/data/WebTrackQ09')
+    Processor.Process(QInName)
+    
+    
+        
+        
+        
+    
