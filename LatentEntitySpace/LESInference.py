376d672b569ab1dc1ee02c33fb08c12cbf5e6323
chenyanxiong <xiongchenyan@gmail.com>
2015-09-02 17:05:31 -0400

    discard debug

--- LatentEntitySpace/LESInference.py
+++ LatentEntitySpace/LESInference.py
@@ -26,10 +26,10 @@ class LESInferencerC(object):
     @classmethod
     def inference(cls,query,doc,lQObj,lDocObj):
         
-        logging.debug('infer q[%s] doc [%s], qobj [%s], doc obj [%s]',\
-                      query,doc.DocNo,
-                      json.dumps([obj.GetId() for obj in lQObj]),\
-                      json.dumps([obj.GetId() for obj in lDocObj]))
+#         logging.debug('infer q[%s] doc [%s], qobj [%s], doc obj [%s]',\
+#                       query,doc.DocNo,
+#                       json.dumps([obj.GetId() for obj in lQObj]),\
+#                       json.dumps([obj.GetId() for obj in lDocObj]))
         
         lDObjSimToQ = cls.CalcObjDistributionOnQuery(lQObj, lDocObj)
         lDocObjScore = cls.CalcDocObjDistribution(doc, lDocObj)
@@ -46,13 +46,13 @@ class LESInferencerC(object):
         lDocObjScore = [LmBaseC.Similarity(ObjLm, DocLm, TermCtfC(), 'cosine') for ObjLm in lObjLm]
         
         Z = float(sum(lDocObjScore))
-        logging.debug('Doc obj dist Z= %f',Z)
+#         logging.debug('Doc obj dist Z= %f',Z)
         if 0 != Z:
             lDocObjScore = [item/Z for item in lDocObjScore]
         else:
             logging.warn('sum of doc obj scores is 0. raw scores:\n%s',json.dumps(lDocObjScore))    
         lDocObjNoScore = zip([obj.GetId() for obj in lDocObj],lDocObjScore)            
-        logging.debug('doc [%s] obj dist:\n%s',doc.DocNo,json.dumps(lDocObjNoScore))
+#         logging.debug('doc [%s] obj dist:\n%s',doc.DocNo,json.dumps(lDocObjNoScore))
         return lDocObjScore
     
     
@@ -68,11 +68,11 @@ class LESInferencerC(object):
         
         
         
-        logging.debug('obj-o obj sim mtx:\n %s',json.dumps(llDObjQObjSim))
-        logging.debug('obj-q sim:\n %s',json.dumps(lDObjSim))
+#         logging.debug('obj-o obj sim mtx:\n %s',json.dumps(llDObjQObjSim))
+#         logging.debug('obj-q sim:\n %s',json.dumps(lDObjSim))
         
         Z = float(sum(lDObjSim))
-        logging.debug('ObjDist on Q Z = %f',Z)
+#         logging.debug('ObjDist on Q Z = %f',Z)
         if Z != 0:
 #             logging.debug('Q obj desp: [%s]',lQObj[0].GetDesp())
 #             logging.debug('doc obj desp: [%s]',lDocObj[0].GetDesp())
