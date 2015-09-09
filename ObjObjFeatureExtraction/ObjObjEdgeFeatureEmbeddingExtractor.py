49f3bd27d1485c1e7b88cee534df528f0fb7b574
chenyanxiong <xiongchenyan@gmail.com>
2015-09-09 11:02:47 -0400

    rename

--- ObjObjFeatureExtraction/ObjObjEdgeFeatureEmbeddingExtractor.py
+++ ObjObjFeatureExtraction/ObjObjEdgeFeatureEmbeddingExtractor.py
@@ -20,7 +20,7 @@ import os
 import logging
 from ObjObjFeatureExtraction.ObjObjEdgeFeatureExtractor import ObjObjEdgeFeatureExtractorC
 
-class ObjObjEdgeFeatureTextSimExtractorC(ObjObjEdgeFeatureExtractorC):
+class ObjObjEdgeFeatureEmbSimExtractorC(ObjObjEdgeFeatureExtractorC):
     
     def Init(self):
         ObjObjEdgeFeatureExtractorC.Init(self)
